4f869ef21c1bfc04539ac8db5bfc53a9a35d948c
Vik Ramanujam <piggyslasher@gmail.com>
2019-01-15 17:39:10 +0800

    Setup prettier & eslint-prettier

--- src/index.js
+++ src/index.js
@@ -1,83 +1,77 @@
 // Modules to control application life and create native browser window
-const { app, BrowserWindow, ipcMain, session } = require("electron");
-const path = require("path");
+const { app, BrowserWindow, ipcMain, session } = require('electron')
 
-const ViewManager = require("./ViewManager");
+const ViewManager = require('./ViewManager')
 
-require("electron-reload")(__dirname, {
-  electron: path.join(__dirname, "node_modules", ".bin", "electron"),
-  hardResetMethod: "exit"
-});
+require('electron-reload')(__dirname, {
+  electron: process.execPath,
+  hardResetMethod: 'exit',
+})
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
-let viewManager;
+let mainWindow
+let viewManager
 
-app.setAppUserModelId(process.execPath);
+app.setAppUserModelId(process.execPath)
 
-const OPTIONS = require("../package.json").config.mnm;
+const OPTIONS = require('../package.json').config.mnm
 
 const resetUserAgent = () => {
-  console.log("am i running");
   session.defaultSession.webRequest.onBeforeSendHeaders((details, callback) => {
-    details.requestHeaders["User-Agent"] = OPTIONS.viewOptions.userAgent;
-    callback({ cancel: false, requestHeaders: details.requestHeaders });
-  });
+    details.requestHeaders['User-Agent'] = OPTIONS.viewOptions.userAgent
+    callback({ cancel: false, requestHeaders: details.requestHeaders })
+  })
 }
 
-
-
 function createWindow() {
-  resetUserAgent();
+  resetUserAgent()
   // Create the browser window.
-  mainWindow = new BrowserWindow(
-    {width: 800, height: 600,
-    })
+  mainWindow = new BrowserWindow({ width: 800, height: 600 })
 
   // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
-  mainWindow.on("closed", () => {
+  mainWindow.on('closed', () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null;
-  });
+    mainWindow = null
+  })
 
-  ipcMain.on("move-to-tab", (evt, idx) => viewManager.moveToView(idx));
-  ipcMain.on("init-services", (evt, services) => {
-    console.log("services to init", services);
-    viewManager = new ViewManager(mainWindow, services);
+  ipcMain.on('move-to-tab', (evt, idx) => viewManager.moveToView(idx))
+  ipcMain.on('init-services', (evt, services) => {
+    console.log('services to init', services)
+    viewManager = new ViewManager(mainWindow, services)
     // viewManager.addView(`file://${__dirname}/prefs.html`, { webPreferences: {nodeIntegration: true}})
-  });
+  })
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on("ready", createWindow);
+app.on('ready', createWindow)
 
 // Quit when all windows are closed.
-app.on("window-all-closed", () => {
+app.on('window-all-closed', () => {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== "darwin") {
-    app.quit();
+  if (process.platform !== 'darwin') {
+    app.quit()
   }
-});
+})
 
-app.on("activate", () => {
+app.on('activate', () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
-    createWindow();
+    createWindow()
   }
-});
+})
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
